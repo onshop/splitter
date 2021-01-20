d917cef691d7712f01f89ad977120e6b115aa429
Ben Parish <ben.parish@onshop.co.uk>
2021-01-20 17:37:23 +0000

    Carriage return

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -22,8 +22,7 @@ contract Splitter {
     function splitDeposit(address payable recipient1, address payable recipient2) external payable{
         require(msg.value > 0, "The value must be greater than 0");
         require(recipient1 != recipient2, "The first recipient is the same as the second recipient");
-        require(msg.sender != recipient1 && msg.sender != recipient2,
-            "The sender cannot also be a recipient");
+        require(msg.sender != recipient1 && msg.sender != recipient2, "The sender cannot also be a recipient");
 
         uint split = msg.value / 2;
 
