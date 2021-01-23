b45e61390ed4e69c2001b6cc46a2c3a86cace215
Ben Parish <ben.parish@onshop.co.uk>
2021-01-23 18:43:41 +0000

    Fixed camelCase issue

--- test/splitter.js
+++ test/splitter.js
@@ -21,8 +21,7 @@ contract('Splitter', async accounts => {
         );
     }
 
-    const [ senderAddress, RecipientOneAddress, RecipientTwoAddress] = accounts;
-
+    const [ senderAddress, recipientOneAddress, recipientTwoAddress] = accounts;
     let splitter;
 
     beforeEach("deploy and prepare", async function() {
@@ -36,11 +35,11 @@ contract('Splitter', async accounts => {
         initSenderEthBalance = toBN(initSenderEthBalance);
 
         const initSenderContractBalance = await splitter.balances(senderAddress);
-        const initRecipientOneContractBalance = await splitter.balances(RecipientOneAddress);
-        const initRecipientTwoContractBalance = await splitter.balances(RecipientTwoAddress);
+        const initRecipientOneContractBalance = await splitter.balances(recipientOneAddress);
+        const initRecipientTwoContractBalance = await splitter.balances(recipientTwoAddress);
 
         // Perform transactions
-        const txObj = await splitter.splitDeposit(RecipientOneAddress, RecipientTwoAddress, {from: senderAddress, value: 5});
+        const txObj = await splitter.splitDeposit(recipientOneAddress, recipientTwoAddress, {from: senderAddress, value: 5});
 
         const cost = await getGasCost(txObj);
         const transfer = toBN(5);
@@ -61,23 +60,23 @@ contract('Splitter', async accounts => {
 
         // Get the actual contract balances
         const aliceContractBalance = await splitter.balances(senderAddress);
-        const bobContractBalance = await splitter.balances(RecipientOneAddress);
-        const carolContractBalance = await splitter.balances(RecipientTwoAddress);
+        const bobContractBalance = await splitter.balances(recipientOneAddress);
+        const carolContractBalance = await splitter.balances(recipientTwoAddress);
 
         assert.strictEqual(aliceContractBalance.toString(10), expectedSenderContractBalance.toString(10));
         assert.strictEqual(bobContractBalance.toString(10), expectedRecipientOneContractBalance.toString(10));
         assert.strictEqual(carolContractBalance.toString(10), expectedRecipientTwoContractBalance.toString(10));
 
         truffleAssert.eventEmitted(txObj, 'Deposit', (ev) => {
-            return  ev.sender === senderAddress && ev.recipient1 === RecipientOneAddress &&
-                    ev.recipient2 === RecipientTwoAddress && ev.amount.toString(10) === "2" &&
+            return  ev.sender === senderAddress && ev.recipient1 === recipientOneAddress &&
+                    ev.recipient2 === recipientTwoAddress && ev.amount.toString(10) === "2" &&
                     ev.remainder.toString(10) === "1";
         }, 'TestEvent should be emitted with correct parameters');
     })
 
     it("Transaction reverts if the deposit amount is zero", async () => {
         await truffleAssert.reverts(
-            splitter.splitDeposit(RecipientTwoAddress, RecipientOneAddress,{from: senderAddress, value: 0}),
+            splitter.splitDeposit(recipientTwoAddress, recipientOneAddress,{from: senderAddress, value: 0}),
             "The value must be greater than 0"
         );
         checkEventNotEmitted();
@@ -85,7 +84,7 @@ contract('Splitter', async accounts => {
 
     it("Transaction reverts if the first recipient is the same as the second recipient", async () => {
         await truffleAssert.reverts(
-            splitter.splitDeposit(RecipientOneAddress, RecipientOneAddress,{from: senderAddress, value: 4}),
+            splitter.splitDeposit(recipientOneAddress, recipientOneAddress,{from: senderAddress, value: 4}),
             "The first recipient is the same as the second recipient"
         );
         checkEventNotEmitted();
@@ -93,7 +92,7 @@ contract('Splitter', async accounts => {
 
     it("Transaction reverts if the sender's address is also a recipient", async () => {
         await truffleAssert.reverts(
-            splitter.splitDeposit(senderAddress, RecipientTwoAddress, {from: senderAddress, value: 4}),
+            splitter.splitDeposit(senderAddress, recipientTwoAddress, {from: senderAddress, value: 4}),
             "The sender cannot also be a recipient"
         );
         checkEventNotEmitted();
@@ -105,23 +104,23 @@ contract('Splitter', async accounts => {
         const withDrawAmount = toBN(2);
 
         // Take snapshot of the recipients ETH balance
-        let initRecipientTwoEthBalance = await web3.eth.getBalance(RecipientTwoAddress);
+        let initRecipientTwoEthBalance = await web3.eth.getBalance(recipientTwoAddress);
         initRecipientTwoEthBalance = toBN(initRecipientTwoEthBalance);
 
         // Deposit into the contract
-        await splitter.splitDeposit(RecipientOneAddress, RecipientTwoAddress, {from: senderAddress, value: depositAmount});
+        await splitter.splitDeposit(recipientOneAddress, recipientTwoAddress, {from: senderAddress, value: depositAmount});
 
         // Take snapshot of the recipients new contract balance
-        let initRecipientTwoContractBalance = await splitter.balances(RecipientTwoAddress);
+        let initRecipientTwoContractBalance = await splitter.balances(recipientTwoAddress);
         initRecipientTwoContractBalance = toBN(initRecipientTwoContractBalance)
 
         // Recipient withdraws
-        const txObj = await splitter.withdraw(withDrawAmount, {from: RecipientTwoAddress});
+        const txObj = await splitter.withdraw(withDrawAmount, {from: recipientTwoAddress});
         const cost = await getGasCost(txObj);
 
         // Get the recipient's new ETH and contract balances
-        const carolEthBalance = await web3.eth.getBalance(RecipientTwoAddress);
-        const carolContractBalance = await splitter.balances(RecipientTwoAddress);
+        const carolEthBalance = await web3.eth.getBalance(recipientTwoAddress);
+        const carolContractBalance = await splitter.balances(recipientTwoAddress);
 
         // Calculate the expected new ETH and contract balances
         const expectedRecipientTwoEthBalance = initRecipientTwoEthBalance.sub(cost).add(withDrawAmount);
@@ -131,7 +130,7 @@ contract('Splitter', async accounts => {
         assert.strictEqual(carolContractBalance.toString(10), expectedRecipientTwoContractBalance.toString(10));
 
         truffleAssert.eventEmitted(txObj, "WithDraw", (ev) => {
-            return  ev.withdrawer === RecipientTwoAddress && ev.amount.toString(10) === "2";
+            return  ev.withdrawer === recipientTwoAddress && ev.amount.toString(10) === "2";
         }, 'TestEvent should be emitted with correct parameters');
 
     });
@@ -141,7 +140,7 @@ contract('Splitter', async accounts => {
         withDrawAmount = web3.utils.toWei(toBN(5), "ether");
 
         await truffleAssert.reverts(
-            splitter.withdraw(withDrawAmount, {from: RecipientTwoAddress}),
+            splitter.withdraw(withDrawAmount, {from: recipientTwoAddress}),
             "There are insufficient funds"
         );
         checkEventNotEmitted();
@@ -150,7 +149,7 @@ contract('Splitter', async accounts => {
     it("Second recipient attempts to withdraw zero", async () => {
 
         await truffleAssert.reverts(
-            splitter.withdraw(toBN(0), {from: RecipientTwoAddress}),
+            splitter.withdraw(toBN(0), {from: recipientTwoAddress}),
             "The value must be greater than 0"
         );
         checkEventNotEmitted();
